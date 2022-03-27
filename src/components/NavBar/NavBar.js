17560893f5b036e170bc0ce3198eb580bf28a0b4
FawziL <fawzi.jlr@gmail.com>
2022-03-27 18:04:21 -0500

    Agregue el NavBar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+import './NavBar.css'
+
+
+const NavBar = () => {
+    return(
+        <nav>
+            <h1>Tutiendaonline</h1>
+            <button>Ford</button>
+            <button>Renault</button>
+            <button>Volkswagen</button>
+            <button>Honda</button>
+            <button>Hyundai</button>
+            <button>Chevrolet</button>
+            <button>Mitsubishi</button>
+        </nav>
+    )
+}
+
+
+export default NavBar
